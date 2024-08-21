c8dcd872672d6afc11c59e3692e8ac629c794a29
Your Name <you@example.com>
2024-08-21 07:43:22 -0700

    modifiy models/media.js models/user.js

--- workwonders/models/media.js
+++ workwonders/models/media.js
@@ -5,7 +5,7 @@ const videoSchema = new Schema({
     title: { type: String, required: true},
     url: { type: Array, required: true},
     createAt: { type: Date, default: Date.now},
-    project: { type: Schema.Types.ObjectId, ref: Project},
+    project: { type: Schema.Types.ObjectId, ref: 'Project'},
 });
 
 module.exports = mongoose.model("Video", videoSchema);
